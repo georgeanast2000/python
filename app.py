a256ca337016a3795346060216ed9079859e1947
georgeanast2000 <georgeanast2000@gmail.com>
2026-05-13 14:23:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
-from flask import Flask, request
-from user_agents import parse # Χρειαζόμαστε αυτό για να "διαβάζει" το User-Agent
-import os
+from flask import Flask , request
+
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    # Παίρνουμε το ακατέργαστο κείμενο
-    ua_string = request.headers.get('User-Agent')
+    glossa = request.headers.get('Accept-Language')
+
+    if 'el' in glossa.lower():
+        minima = "Γειά σου" 
+    else:
+        minima= "Hi"
+
     
-    # Το "μεταφράζουμε" σε κάτι κατανοητό
-    user_agent = parse(ua_string)
-    
-    # Διαλέγουμε τι θέλουμε να δείξουμε
-    browser = user_agent.browser.family      # π.χ. Chrome
-    os_system = user_agent.os.family         # π.χ. Windows ή Android
-    device = user_agent.device.family        # π.χ. iPhone ή Generic Smartphone
-    
+            
     return f'''
-        <div style="font-family: sans-serif; text-align: center; margin-top: 50px;">
-            <h1> Η ταυτότητά σου </h1>
-            <p> 🌐 <b>Browser:</b> {browser} </p>
-            <p> 💻 <b>Λειτουργικό:</b> {os_system} </p>
-            <p> 📱 <b>Συσκευή:</b> {device} </p>
-            <hr>
-            <p style="color: gray;">Ολόκληρο το User-Agent: <br> <small>{ua_string}</small></p>
-        </div>
+        <h1> {minima}</h1>
+        <p> Η γλώσσα του browser είναι {glossa}</p>
+        </body>
         '''
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
